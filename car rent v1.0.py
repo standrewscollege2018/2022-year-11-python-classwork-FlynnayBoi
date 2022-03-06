200645bb4beadff47c4874c7de0d8441199f6ee0
FlynnB <78829192+FlynnayBoi@users.noreply.github.com>
2022-03-07 12:36:53 +1300

    Car Rental Practice Task
    cars and status

---
+++ car rent v1.0.py
@@ -0,0 +1,47 @@
+#Car Rental
+print('Welcome to the University Retal System')
+print('Vehicles avaliable to rent are:')
+car_list = []
+
+#Varibles that are the cars and whether they are avaliable
+suz_van = f"Suzuki Van - 2 Seats"
+suzvan_av = True
+toy_cor = "Toyota Corolla - 4 Seats"
+toycor_av = True
+hon_crv = "Honda CRV - 4 Seats"
+honcrv = True
+suz_swi = "Suzuki Swift - 4 Seats"
+suzswi_av = True
+mit_air = "Mitzubishi Airtrek - 4 Seats"
+mitair_av = True
+nisdc_ute = "Nissan DC Ute - 4 Seats"
+nisdcute_av = True
+toy_pre = "Toyota Previa - 7 Seats"
+toypre_av = True
+toyhi_ace1 = "Toyota HI Ace 1 - 12 Seats"
+toyhiace1_av = True
+toyhi_ace2 = "Toyota HI Ace 2 - 12 Seats"
+toyhiace2_av = True
+
+#Adding the cars to a list
+car_list.append(suz_van)
+car_list.append(toy_cor)
+car_list.append(hon_crv)
+car_list.append(suz_swi)
+car_list.append(mit_air)
+car_list.append(nisdc_ute)
+car_list.append(toy_pre)
+car_list.append(toyhi_ace1)
+car_list.append(toyhi_ace2)
+
+#Printing the cars to see the options
+print(car_list[0])
+print(car_list[1])
+print(car_list[2])
+print(car_list[3])
+print(car_list[4])
+print(car_list[5])
+print(car_list[6])
+print(car_list[7])
+print(car_list[8])
+
